bdb0d61fd59b898e3bce80cf014cc061ef4a7015
wue1atwit <wue1@wit.edu>
2024-04-07 18:52:22 -0400

    Edit/Delete Employee in Modal
    Modal now incorporates editting and deleting an employee

--- src/renderer/src/Components/EmployeeManagement.jsx
+++ src/renderer/src/Components/EmployeeManagement.jsx
@@ -11,10 +11,17 @@ const EmployeeListItem = ({
   first_name,
   last_name,
   authority_level,
+  checkedEmployees,
   setCheckedEmployees,
   disableCheck,
 }) => {
   const [checked, setChecked] = useState(false);
+
+  useEffect(() => {
+    if (!checkedEmployees.includes(pin)) {
+      setChecked(false);
+    }
+  }, [checkedEmployees]);
 
   const handleCheckChange = (event) => {
     event.stopPropagation();
@@ -56,7 +63,14 @@ const EmployeeListItem = ({
   );
 };
 
-const EmployeeModal = ({ show, setShow, isEditting }) => {
+const EmployeeModal = ({
+  show,
+  setShow,
+  setCheckedEmployees,
+  isEditting,
+  editUser,
+}) => {
+  const [user, setUser] = useState(null);
   const [newEmployee, setNewEmployee] = useState({
     pin: "",
     firstName: "",
@@ -70,6 +84,32 @@ const EmployeeModal = ({ show, setShow, isEditting }) => {
     authorityLevel: { invalid: false, msg: "" },
   });
   const [formSubmitted, setFormSubmitted] = useState(false);
+
+  useEffect(() => {
+    sendSQL(`SELECT * FROM employees WHERE pin='${editUser}'`).then(
+      (response) => {
+        if (response.length > 0) {
+          console.log(response);
+          const employee = response[0];
+          setUser(employee);
+          setNewEmployee({
+            pin: employee.pin,
+            firstName: employee.first_name,
+            lastName: employee.last_name,
+            authorityLevel: employee.authority_level,
+          });
+        } else {
+          setUser(null);
+          setNewEmployee({
+            pin: "",
+            firstName: "",
+            lastName: "",
+            authorityLevel: null,
+          });
+        }
+      }
+    );
+  }, [editUser]);
 
   const clearValidation = () => {
     setValidation({
@@ -90,22 +130,35 @@ const EmployeeModal = ({ show, setShow, isEditting }) => {
       newEmployee.authorityLevel
     ) {
       clearValidation();
-      const sql = `INSERT INTO employees (pin, first_name, last_name, authority_level) VALUES('${newEmployee.pin}', '${newEmployee.firstName}', '${newEmployee.lastName}', ${newEmployee.authorityLevel})`;
+
       const employees = await sendSQL(
         `SELECT pin FROM employees WHERE pin='${newEmployee.pin}'`
       );
-
       const employeeExist = (await employees.length) > 0;
 
-      if (employeeExist) {
+      let oldPin = undefined;
+      if (isEditting) {
+        oldPin = employees[0].pin;
+      }
+
+      const insertSQLCommand = `INSERT INTO employees (pin, first_name, last_name, authority_level) VALUES('${newEmployee.pin}', '${newEmployee.firstName}', '${newEmployee.lastName}', ${newEmployee.authorityLevel})`;
+      const updateSQLCommand = `UPDATE employees SET pin='${newEmployee.pin}', first_name='${newEmployee.firstName}', last_name='${newEmployee.lastName}', authority_level=${newEmployee.authorityLevel} WHERE pin='${oldPin}'`;
+
+      if (employeeExist && !isEditting) {
         setValidation({
           ...validation,
           pin: { invalid: true, msg: "PIN already exist." },
         });
       } else {
-        const response = await insertSQL(sql);
-        console.log(await response);
-        setShow(false);
+        if (isEditting) {
+          const response = await sendSQL(updateSQLCommand);
+          console.log(response);
+        } else {
+          const response = await insertSQL(insertSQLCommand);
+          console.log(response);
+        }
+        setShow({ show: false, isEdit: false });
+        setCheckedEmployees([]);
       }
     } else {
       let newValidation = { ...validation };
@@ -159,17 +212,28 @@ const EmployeeModal = ({ show, setShow, isEditting }) => {
       setValidation(newValidation);
     }
   };
+
+  const handleDeleteEmployee = async (event) => {
+    const response = await insertSQL(
+      `DELETE FROM employees WHERE pin='${editUser}'`
+    );
+    setShow({ show: false, inEdit: false });
+    setCheckedEmployees([]);
+    return response;
+  };
+
   return (
     <Modal
       show={show}
       onHide={() => {
-        setShow(false);
+        setShow((prev) => ({ show: false, inEdit: false }));
         setNewEmployee({
           pin: "",
           firstName: "",
           lastName: "",
           authorityLevel: null,
         });
+        setCheckedEmployees([]);
       }}
     >
       <Modal.Header closeButton>
@@ -182,6 +246,7 @@ const EmployeeModal = ({ show, setShow, isEditting }) => {
             <Form.Control
               type="number"
               placeholder="0000"
+              defaultValue={isEditting ? `${user.pin}` : ""}
               isInvalid={formSubmitted && validation.pin.invalid}
               required
               onChange={(event) =>
@@ -198,6 +263,7 @@ const EmployeeModal = ({ show, setShow, isEditting }) => {
             <Form.Control
               type="text"
               placeholder="John"
+              defaultValue={isEditting ? `${user.first_name}` : ""}
               isInvalid={formSubmitted && validation.firstName.invalid}
               required
               onChange={(event) =>
@@ -218,6 +284,7 @@ const EmployeeModal = ({ show, setShow, isEditting }) => {
             <Form.Control
               type="text"
               placeholder="Doe"
+              defaultValue={isEditting ? `${user.last_name}` : ""}
               isInvalid={formSubmitted && validation.lastName.invalid}
               required
               onChange={(event) =>
@@ -231,6 +298,7 @@ const EmployeeModal = ({ show, setShow, isEditting }) => {
           <Form.Group>
             <Form.Label className="required">Authority Level</Form.Label>
             <Form.Select
+              defaultValue={isEditting ? user.authority_level : "Please select"}
               isInvalid={formSubmitted && validation.authorityLevel.invalid}
               required
               onChange={(event) =>
@@ -252,7 +320,11 @@ const EmployeeModal = ({ show, setShow, isEditting }) => {
         </Form>
       </Modal.Body>
       <Modal.Footer>
-        {isEditting && <Button variant="danger">Delete</Button>}
+        {isEditting && (
+          <Button variant="danger" onClick={handleDeleteEmployee}>
+            Delete
+          </Button>
+        )}
         <Button onClick={handleAddEmployee}>Ok</Button>
       </Modal.Footer>
     </Modal>
@@ -262,7 +334,7 @@ const EmployeeModal = ({ show, setShow, isEditting }) => {
 const EmployeeManagement = ({ user }) => {
   const [employees, setEmployees] = useState([]);
   const [checkedEmployees, setCheckedEmployees] = useState([]);
-  const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState({ show: false, isEdit: false });
 
   useEffect(() => {
     getEmployees();
@@ -272,6 +344,7 @@ const EmployeeManagement = ({ user }) => {
     getEmployees();
   }, [showModal]);
 
+  console.log(showModal);
   useEffect(() => {
     console.log(checkedEmployees.map((employee) => `'${employee}'`).join(","));
   }, [checkedEmployees]);
@@ -296,7 +369,13 @@ const EmployeeManagement = ({ user }) => {
 
   return (
     <Container className="employee-management-container">
-      <EmployeeModal show={showModal} setShow={setShowModal} />
+      <EmployeeModal
+        show={showModal.show}
+        setShow={setShowModal}
+        setCheckedEmployees={setCheckedEmployees}
+        isEditting={showModal.isEdit}
+        editUser={checkedEmployees.length === 1 && checkedEmployees[0]}
+      />
       <Row className="employee-list-header">
         <Col className="list-header-item" xs={1}></Col>
         <Col className="list-header-item">PIN</Col>
@@ -304,29 +383,45 @@ const EmployeeManagement = ({ user }) => {
         <Col className="list-header-item">Last Name</Col>
         <Col className="list-header-item">Authority</Col>
       </Row>
-      {employees.map((employee) => (
-        <EmployeeListItem
-          key={employee.pin}
-          pin={employee.pin}
-          first_name={employee.first_name}
-          last_name={employee.last_name}
-          authority_level={employee.authority_level}
-          checkedEmployees={checkedEmployees}
-          setCheckedEmployees={setCheckedEmployees}
-          disableCheck={user.pin === employee.pin}
-        />
-      ))}
+      {employees.map((employee) => {
+        console.log(
+          `${user.authority_level} <= ${employee.authority_level} ${
+            user.authority_level <= employee.authority_level
+          }`
+        );
+        return (
+          <EmployeeListItem
+            key={employee.pin}
+            pin={employee.pin}
+            first_name={employee.first_name}
+            last_name={employee.last_name}
+            authority_level={employee.authority_level}
+            checkedEmployees={checkedEmployees}
+            setCheckedEmployees={setCheckedEmployees}
+            disableCheck={
+              user.pin === employee.pin ||
+              user.authority_level <= employee.authority_level
+            }
+          />
+        );
+      })}
       <div className="employee-button-container">
         <div className="employee-button-group">
           {checkedEmployees.length === 0 && (
-            <Button className="icon-button" onClick={() => setShowModal(true)}>
+            <Button
+              className="icon-button"
+              onClick={() => setShowModal((prev) => ({ ...prev, show: true }))}
+            >
               <PlusSquare size={16} />
               Add Employee
             </Button>
           )}
 
           {checkedEmployees.length === 1 && (
-            <Button className="icon-button">
+            <Button
+              className="icon-button"
+              onClick={() => setShowModal({ show: true, isEdit: true })}
+            >
               <PencilSquare size={16} />
               Edit Employee
             </Button>
